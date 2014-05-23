1fab8335435f4426014b5d313b6da835b508a0ff
clebert suconic <clebert.suconic@gmail.com>
2014-05-23 11:35:44 -0400

    https://bugzilla.redhat.com/show_bug.cgi?id=1096942 - Fixing Clustered Grouping issues Collaboration Clebert / Justin and Andy
    (cherry picked from commit be944e1639d3c7de68f50c4626890cfeffa2cd7e)

--- hornetq-server/src/main/java/org/hornetq/core/server/impl/HornetQServerImpl.java
+++ hornetq-server/src/main/java/org/hornetq/core/server/impl/HornetQServerImpl.java
@@ -603,7 +603,6 @@ public class HornetQServerImpl implements HornetQServer
          {
             managementService.removeNotificationListener(groupingHandler);
             groupingHandler.stop();
-            groupingHandler = null;
          }
          stopComponent(clusterManager);
          freezeConnections();
@@ -1216,7 +1215,17 @@ public class HornetQServerImpl implements HornetQServer
 
    public void setGroupingHandler(final GroupingHandler groupingHandler)
    {
+      if (this.groupingHandler != null && managementService != null)
+      {
+         // Removing old groupNotification
+         managementService.removeNotificationListener(this.groupingHandler);
+      }
       this.groupingHandler = groupingHandler;
+      if (managementService != null)
+      {
+         managementService.addNotificationListener(this.groupingHandler);
+      }
+
    }
 
    public GroupingHandler getGroupingHandler()
@@ -2146,10 +2155,11 @@ public class HornetQServerImpl implements HornetQServer
          else
          {
             groupingHandler1 =
-               new RemoteGroupingHandler(managementService,
-                                         config.getName(),
-                                         config.getAddress(),
-                                         config.getTimeout());
+                     new RemoteGroupingHandler(executorFactory, managementService,
+                        config.getName(),
+                        config.getAddress(),
+                        config.getTimeout(),
+                        config.getGroupTimeout());
          }
 
          this.groupingHandler = groupingHandler1;
