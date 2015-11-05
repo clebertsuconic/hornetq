3a0eb80851df03d57ef96dc17092d73962d263f9
Clebert Suconic <clebertsuconic@apache.org>
2015-11-04 22:19:41 -0500

    new test

---
+++ tests/byteman-tests/src/test/java/org/hornetq/byteman/tests/ConcurrentDeliveryCancelTest.java
@@ -0,0 +1,163 @@
+/**
+ * Licensed to the Apache Software Foundation (ASF) under one or more
+ * contributor license agreements. See the NOTICE file distributed with
+ * this work for additional information regarding copyright ownership.
+ * The ASF licenses this file to You under the Apache License, Version 2.0
+ * (the "License"); you may not use this file except in compliance with
+ * the License. You may obtain a copy of the License at
+ * <p/>
+ * http://www.apache.org/licenses/LICENSE-2.0
+ * <p/>
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package org.hornetq.byteman.tests;
+
+import javax.jms.Connection;
+import javax.jms.MessageConsumer;
+import javax.jms.MessageProducer;
+import javax.jms.Queue;
+import javax.jms.Session;
+import javax.jms.TextMessage;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.concurrent.atomic.AtomicInteger;
+
+import org.hornetq.api.core.HornetQException;
+import org.hornetq.api.core.TransportConfiguration;
+import org.hornetq.api.jms.HornetQJMSClient;
+import org.hornetq.api.jms.JMSFactoryType;
+import org.hornetq.core.client.impl.ClientSessionInternal;
+import org.hornetq.core.server.ServerSession;
+import org.hornetq.jms.client.HornetQConnectionFactory;
+import org.hornetq.jms.client.HornetQSession;
+import org.hornetq.tests.util.JMSTestBase;
+import org.hornetq.tests.util.RandomUtil;
+import org.junit.Assert;
+import org.junit.Test;
+
+public class ConcurrentDeliveryCancelTest extends JMSTestBase
+{
+
+   @Test
+   public void testConcurrentCancels() throws Exception
+   {
+
+      List<TransportConfiguration> connectorConfigs = new ArrayList<TransportConfiguration>();
+      HornetQConnectionFactory cf = HornetQJMSClient.createConnectionFactoryWithHA(JMSFactoryType.CF, new TransportConfiguration(NETTY_CONNECTOR_FACTORY));
+      cf.setReconnectAttempts(0);
+      cf.setRetryInterval(10);
+
+
+      System.out.println(".....");
+      for (ServerSession srvSess : server.getSessions())
+      {
+         System.out.println(srvSess);
+      }
+
+      String queueName = RandomUtil.randomString();
+      Queue queue = createQueue(queueName);
+
+      Connection connection = cf.createConnection();
+      Session session = connection.createSession(true, Session.SESSION_TRANSACTED);
+      MessageProducer producer = session.createProducer(queue);
+
+      int numberOfMessages = 1000;
+      for (int i = 0; i < numberOfMessages; i++)
+      {
+         TextMessage msg = session.createTextMessage("message " + i);
+         msg.setIntProperty("i", i);
+         producer.send(msg);
+      }
+      session.commit();
+
+      connection.close();
+
+      for (int i = 0; i < 100; i++)
+      {
+         connection = cf.createConnection();
+         connection.setClientID("myID");
+
+         session = connection.createSession(true, Session.SESSION_TRANSACTED);
+
+         connection.start();
+
+         MessageConsumer consumer = session.createConsumer(queue);
+
+         Assert.assertNotNull(consumer.receiveNoWait());
+
+         System.out.println(".....");
+
+         // We will force now the failure simultaneously from several places
+         for (ServerSession srvSess : server.getSessions())
+         {
+            System.out.println(srvSess);
+            srvSess.getRemotingConnection().fail(new HornetQException("fail"));
+         }
+         ClientSessionInternal impl = (ClientSessionInternal) ((HornetQSession)session).getCoreSession();
+         impl.getConnection().fail(new HornetQException("failure"));
+         connection.close();
+      }
+
+      connection = cf.createConnection();
+      connection.setClientID("myID");
+
+      connection.start();
+      session = connection.createSession(false, Session.AUTO_ACKNOWLEDGE);
+      MessageConsumer consumer = session.createConsumer(queue);
+      HashMap<Integer, AtomicInteger> mapCount = new HashMap<Integer, AtomicInteger>();
+
+      while (true)
+      {
+         TextMessage message = (TextMessage)consumer.receiveNoWait();
+         if (message == null)
+         {
+            break;
+         }
+
+         Integer value = message.getIntProperty("i");
+
+         System.out.println("received message = " + value);
+
+         AtomicInteger count = mapCount.get(value);
+         if (count == null)
+         {
+            count = new AtomicInteger(0);
+            mapCount.put(message.getIntProperty("i"), count);
+         }
+         else
+         {
+            System.out.println("Duplicated message");
+         }
+
+         count.incrementAndGet();
+      }
+
+      boolean failed = false;
+      for (int i = 0; i < numberOfMessages; i++)
+      {
+         AtomicInteger count = mapCount.get(i);
+         if (count == null)
+         {
+            System.out.println("Message " + i + " not received");
+            failed = true;
+         }
+         else if (count.get() > 1)
+         {
+            System.out.println("Message " + i + " received " + count.get() + " times");
+            failed = true;
+         }
+      }
+
+      Assert.assertFalse("test failed, look at the system.out of the test for more infomration", failed);
+
+      connection.close();
+
+
+   }
+}
